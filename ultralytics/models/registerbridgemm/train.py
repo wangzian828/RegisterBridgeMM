da5a051ff7d61949c5fe002cc6af285b68287ac6
Zian Wang <3481361480@qq.com>
2026-03-21 17:30:08 +0800

    fix: disable rect val loader in registerbridgemm trainer

--- ultralytics/models/registerbridgemm/train.py
+++ ultralytics/models/registerbridgemm/train.py
@@ -3,6 +3,8 @@ from ultralytics.nn.tasks_registerbridge import RegisterBridgeDetectionModel
 from ultralytics.utils import RANK
 import ultralytics.engine.trainer as trainer_module
 from copy import copy
+from ultralytics.data.build import build_yolo_dataset
+from ultralytics.nn.tasks import de_parallel
 
 from .val import RegisterBridgeMMValidator
 
@@ -58,6 +60,26 @@ class RegisterBridgeMMTrainer(MultiModalDetectionTrainer):
     def _setup_train(self, world_size):
         super()._setup_train(world_size)
         self._preserve_registerbridge_freeze()
+
+    def build_dataset(self, img_path, mode="train", batch=None):
+        self.multimodal_config = self._parse_multimodal_config()
+        self._validate_modality_compatibility()
+        x_modality = [m for m in self.multimodal_config["models"] if m != "rgb"][0]
+        x_modality_dir = self.multimodal_config["modalities"][x_modality]
+        gs = max(int(de_parallel(self.model).stride.max() if self.model else 0), 32)
+        return build_yolo_dataset(
+            self.args,
+            img_path,
+            batch,
+            self.data,
+            mode=mode,
+            rect=False if mode == "val" else mode == "val",
+            stride=gs,
+            multi_modal_image=True,
+            x_modality=x_modality,
+            x_modality_dir=x_modality_dir,
+            enable_self_modal_generation=getattr(self.args, "enable_self_modal_generation", False),
+        )
 
     def get_validator(self):
         self.loss_names = "box_loss", "cls_loss", "dfl_loss"
